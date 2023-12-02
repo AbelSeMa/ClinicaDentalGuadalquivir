9982d5c4937a607fa1a5cdd8e7c27cf17566b83e
AbelSM <sexto_abel@hotmai.com>
2023-12-02 19:41:52 +0100

    tabla services

--- database/migrations/2023_12_02_183811_create_services_table.php
+++ database/migrations/2023_12_02_183811_create_services_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('Service');
+            $table->text('Description');
             $table->timestamps();
         });
     }
