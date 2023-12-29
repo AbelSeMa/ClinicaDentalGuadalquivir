c0bacaf709aaed252704c279909265d69e666557
AbelSM <sexto_abel@hotmai.com>
2023-12-29 18:38:44 +0100

    correción de la tabla en tamaño movil

--- resources/views/userDashboard.blade.php
+++ resources/views/userDashboard.blade.php
@@ -46,7 +46,7 @@
     <h2 class="text-3xl  font-extrabold  text-gray-400 dark:text-gray-500 text-center">Citas pendientes</h2>
     <div class="container">
         <div class="relative overflow-x-auto">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <table class="w-full table-fixed text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="px-6 py-3">
@@ -67,7 +67,7 @@
                     @foreach ($citas as $cita)
                         <tr class="text-center bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <td scope="row"
-                                class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                class="text-center px-6 py-4 font-medium text-gray-900 dark:text-white">
                                 {{ \Carbon\Carbon::parse($cita->date)->formatLocalized('%d %B %Y') }}
                             </td>
                             <td class="px-6 py-4">
@@ -152,6 +152,7 @@
 
                 </tbody>
             </table>
+            {{$citas->links()}}
         </div>
     </div>
 @endsection
